eb465ce633400c41e41887cdc7e6e775a8493263
Tesla <you@example.com>
2019-01-29 07:12:06 +0300

    изменено:      usb.c

--- soft/usb.c
+++ soft/usb.c
@@ -3,14 +3,29 @@
 
 int main(int argc, char *argv[]) {
 
+	struct libusb_device_descriptor desc;
+
 	libusb_context *context;
 	libusb_init(&context);
 
 	ssize_t sz;
-	sz = libusb_get_device_list(context, list);
+	libusb_device **list;
+	sz = libusb_get_device_list(context, &list);
+
+	for (ssize_t i = 0; i < sz; i++) {
+
+		int status = libusb_get_device_descriptor(list[i], &desc);
+		if (status >= 0) {
+			printf("%04X:%04X\n", desc.idVendor, desc.idProduct);
+			printf("bDeviceClass %d\n", desc.bDeviceClass);
+			printf("bDeviceSubClass %d\n", desc.bDeviceSubClass);
+			printf("\n");
+		}
+
+	}
 
 
-	libusb_free_device_list(list, 0);
+	libusb_free_device_list(list, 1);
 
 	libusb_exit(context); 	
 	return 0;
