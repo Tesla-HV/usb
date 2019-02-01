c231ed0d54ea93e9a7dd47f71487fc26ca0f8163
Tesla <you@example.com>
2019-02-01 07:53:02 +0300

    Сделана тестовая прошивка и демка для работы с ней

--- fw/fw.c
+++ fw/fw.c
@@ -1,32 +1,71 @@
 #include "fx2regs.h"
 
+#define MSB(x) (((int)(x) >> 8) & 0xFF)
+#define LSB(x) ((int)(x) & 0xFF)
+
+// Я не знаю, сколько тут нужно nop-ов, но думаю, что 16 хватит
+#define SYNCDELAY __asm__("nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;")
+
+char *data1 = "Hello World!";
+char *data2 = "Goodbye!";
+
+void send_string(const unsigned char *data) {
+	int len = 0;
+	AUTOPTRH2 = MSB(&EP6FIFOBUF);
+	AUTOPTRL2 = LSB(&EP6FIFOBUF);
+	len = 0;
+	do {
+		EXTAUTODAT2 = *data;
+		len++;	
+	} while(*(data++) != 0);
+	EP6BCH = MSB(len);
+	SYNCDELAY;
+	EP6BCL = LSB(len);
+	SYNCDELAY;
+}
+
 void main() {
 
-	unsigned int i;
+	unsigned int i, j, len;
+	char *data_ptr;
 
-
-	USBCS |= bmDISCON;
-	for (i = 0; i < 65535; i++) {
-		__asm__ ("nop");
-	}
-	USBCS &= ~bmDISCON;
-
-	USBIE = 0;
-
+	// Переключаем тактовую частоту процессора на 48 МГц
+	CPUCS = ((CPUCS & ~bmCLKSPD) | bmCLKSPD1);
 
 	while(1) {
 
+		// Висим в цикле пока в буфере EP6-IN нет свободного места
+		while(EP2468STAT & bmEP6FULL);
+
+		// Отправляем привет
+		send_string(data1);
+
+		// Светодиод гори!
 		OEC |= 1;
 
-		for (i = 0; i < 65535; i++) {
-			__asm__ ("nop");
+		// Ничего не делаем 1048576 раз (для задержки)
+		for (i = 0; i < 1024; i++) {
+			for (j = 0; j < 1024; j++) {
+				__asm__ ("nop");
+			}
 		}
+	
+		// Висим в цикле пока в буфере EP6-IN нет свободного места
+		while(EP2468STAT & bmEP6FULL);
 
+		// Отправляем пока
+		send_string(data2);
+
+		// Светодиод погасни!
 		OEC &= ~0x01;
 
-		for (i = 0; i < 65535; i++) {
-			__asm__ ("nop");
+		// Ничего не делаем 1048576 раз (для задержки)
+		for (i = 0; i < 1024; i++) {
+			for (j = 0; j < 1024; j++) {
+				__asm__ ("nop");
+			}
 		}
+
 	}
 
 }
--- soft/usb.c
+++ soft/usb.c
@@ -1,34 +1,114 @@
 #include <stdio.h>
+
+#include <stdbool.h>
 #include "libusb.h"
+
+#define RW_INTERNAL     0xA0
+#define RW_MEMORY       0xA3
+
+
+int work(libusb_device_handle *device) {
+
+	unsigned char data[128];
+
+	int status;
+	int transferred;
+
+	while (1) {
+		status = libusb_bulk_transfer(device, 0x86, data, sizeof(data), &transferred, 1000);
+		if (status < 0) {
+			fprintf(stderr, "libusb_bulk_transfer() failed: %s\n", libusb_error_name(status));
+			return -1;
+		}
+
+		printf("%s\n", data);
+	}
+
+	return 0;
+}
+
+
 
 int main(int argc, char *argv[]) {
 
 	struct libusb_device_descriptor desc;
+	struct libusb_context *context;
+	libusb_device_handle *device;
+	int status;
 
-	libusb_context *context;
-	libusb_init(&context);
+	status = libusb_init(&context);
+	if (status < 0) {
+		fprintf(stderr, "libusb_init() failed: %s\n", libusb_error_name(status));
+		return -1;
+	}
+
 
 	ssize_t sz;
-	libusb_device **list;
+	struct libusb_device **list;
 	sz = libusb_get_device_list(context, &list);
+	if (sz < 0) {
+		fprintf(stderr, "libusb_get_device_list() failed: %s\n", libusb_error_name(sz));
+		goto err;
+	}
+
+
+	struct libusb_device *dev = NULL;
 
 	for (ssize_t i = 0; i < sz; i++) {
 
-		int status = libusb_get_device_descriptor(list[i], &desc);
+		status = libusb_get_device_descriptor(list[i], &desc);
 		if (status >= 0) {
-			printf("%04X:%04X\n", desc.idVendor, desc.idProduct);
-			printf("bDeviceClass %d\n", desc.bDeviceClass);
-			printf("bDeviceSubClass %d\n", desc.bDeviceSubClass);
-			printf("\n");
+			//printf("%04X:%04X\n", desc.idVendor, desc.idProduct);
+			if (desc.idVendor == 0x04B4 && desc.idProduct == 0x8613) {
+				dev = list[i];
+				break;
+			}
 		}
 
 	}
 
 
-	libusb_free_device_list(list, 1);
+	if (dev == NULL) {
+		fprintf(stderr, "Device not found\n");
+		libusb_free_device_list(list, 1);
+		goto err;
+	}
 
-	libusb_exit(context); 	
+	status = libusb_open(dev, &device);
+	libusb_free_device_list(list, 1);
+	if (status < 0) {
+		fprintf(stderr, "libusb_open() failed: %s\n", libusb_error_name(status));
+		goto err;
+	}
+
+	libusb_set_auto_detach_kernel_driver(device, 1);
+	status = libusb_claim_interface(device, 0);
+	if (status != LIBUSB_SUCCESS) {
+		libusb_close(device);
+		fprintf(stderr, "libusb_claim_interface failed: %s\n", libusb_error_name(status));
+		goto err;
+	}
+
+	status = libusb_set_interface_alt_setting(device, 0, 1);
+	if (status != LIBUSB_SUCCESS) {
+		libusb_close(device);
+		fprintf(stderr, "libusb_set_interface_alt_setting failed: %s\n", libusb_error_name(status));
+		goto err;
+	}
+
+	work(device);
+
+
+	libusb_release_interface(device, 0);
+	libusb_close(device);
+
+
+	libusb_exit(context);
 	return 0;
+
+err:
+	libusb_exit(context);
+	return -1;
 }
 
 
